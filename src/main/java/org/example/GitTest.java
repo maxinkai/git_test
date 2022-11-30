a3208277e3cb44637018af86e82f532822ad350c
maxinkai <maxinkai@163.com>
2022-11-30 22:06:50 +0800

    hot-fix commit1

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("123");
         System.out.println("123");
         System.out.println("123");
-        System.out.println("123");
+        System.out.println("123123");
     }
 }
