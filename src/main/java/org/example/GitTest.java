d121b409e0440399d6f9a815b4dc3c2facdc94f1
maxinkai <maxinkai@163.com>
2022-11-30 21:59:06 +0800

    third commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -3,5 +3,8 @@ package org.example;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
     }
 }
